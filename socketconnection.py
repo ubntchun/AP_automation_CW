a744a95bc8dbfb53dec3c2c8998f557a78a4a737
c33wang@gmail.com <templar011>
2015-11-08 20:12:25 -0800

    controlling multiple android devices

--- socketconnection.py
+++ socketconnection.py
@@ -1,20 +1,39 @@
 import socket
  
-HOST = "192.168.4.1"
 PORT = 8080
- 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
- 
-sock.sendall("two'\n")
-data = sock.recv(1024)
-print "1)", data
- 
-if ( data == "olleH\n" ):
-    sock.sendall("Bye\n")
-    data = sock.recv(1024)
-    print "2)", data
- 
-    if (data == "eyB}\n"):
-        sock.close()
-        print "Socket closed"
+
+
+
+def play_youtube(ip,num):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip, PORT))
+    sock.sendall(num + "\n")
+
+play_youtube("192.168.5.1", "three")
+play_youtube("192.168.5.2", "three")
+play_youtube("192.168.5.3", "three")
+play_youtube("192.168.5.4", "three")
+play_youtube("192.168.5.5", "three")
+
+
+
+
+
+
+
+
+
+
+
+
+# data = sock.recv(1024)
+# print "1)", data
+#
+# if ( data == "olleH\n" ):
+#     sock.sendall("Bye\n")
+#     data = sock.recv(1024)
+#     print "2)", data
+#
+#     if (data == "eyB}\n"):
+#         sock.close()
+#         print "Socket closed"
